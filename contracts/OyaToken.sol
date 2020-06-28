f7367d0f52086aeba7d4adbccc994283c27738f1
pemulis <john.d.shutt@gmail.com>
2020-06-28 11:41:55 -0700

    feat: update oya token and sample script

--- contracts/OyaToken.sol
+++ contracts/OyaToken.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.6.2;
 import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
 
-contract Token is ERC20PresetMinterPauser {
-  constructor() public ERC20PresetMinterPauser("Oya Token", "OYA") {}
+contract OyaToken is ERC20PresetMinterPauser {
+  constructor() public ERC20PresetMinterPauser("Oya", "OYA") {}
 }
