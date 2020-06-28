26d9afc5a900a2d29cdeb14cb42fa63de1d411f6
pemulis <john.d.shutt@gmail.com>
2020-06-28 12:32:36 -0700

    feat: begin filling in ropsten variables and install dotenv

--- buidler.config.js
+++ buidler.config.js
@@ -1,6 +1,16 @@
 usePlugin("@nomiclabs/buidler-waffle");
 usePlugin("buidler-gas-reporter");
 
+// Go to https://infura.io/ and create a new project
+// Replace this with your Infura project ID
+const INFURA_PROJECT_ID = "YOUR INFURA PROJECT ID";
+
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const ROPSTEN_PRIVATE_KEY = "YOUR ROPSTEN PRIVATE KEY";
+
 // This is a sample Buidler task. To learn how to create your own go to
 // https://buidler.dev/guides/create-task.html
 task("accounts", "Prints the list of accounts", async () => {
@@ -32,6 +42,10 @@ module.exports = {
       accounts: {
         mnemonic: "SOME MNEMONIC TEXT HERE"
       },*/
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
     },
   },
 };
